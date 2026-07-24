45f9f80a5aea5d78fd97d0359e58b493061f5e81
Ralle Palaveev <90763847+rallepalaveev@users.noreply.github.com>
2026-07-24 13:49:00 +0300

    Enable support for PICO_RP2350_A2

--- src/boards/include/boards/clintech_cpb.h
+++ src/boards/include/boards/clintech_cpb.h
@@ -79,9 +79,9 @@ pico_board_cmake_set_default(PICO_FLASH_SIZE_BYTES, (2 * 1024 * 1024))
 #define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
 #endif
 
-pico_board_cmake_set_default(PICO_RP2350_A2_SUPPORTED, 0)
+pico_board_cmake_set_default(PICO_RP2350_A2_SUPPORTED, 1)
 #ifndef PICO_RP2350_A2_SUPPORTED
-#define PICO_RP2350_A2_SUPPORTED 0
+#define PICO_RP2350_A2_SUPPORTED 1
 #endif
 
 #endif
